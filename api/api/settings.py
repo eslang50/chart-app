9ea28f1f65a362ff8fccaffa067455df38e86ab6
Ethan <ethanliu50@gmail.com>
2025-01-05 16:43:15 -0500

    updated settings and migration

--- api/api/settings.py
+++ api/api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-0rh3kcce9b0aq1%+_q#%6^75+b%bs07c(=#inh4u-tm+i1unpf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['stocksnap-backend.onrender.com']
+ALLOWED_HOSTS = ['stocksnap-backend.onrender.com', 'localhost']
 
 
 # Application definition
